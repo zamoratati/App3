17552a0b5c18b4d47aa2043ed679a5cd5ff3794a
Tatiana Zamora <Tatiana Zamora@DESKTOP-F9QVDOT>
2023-03-03 19:50:46 -0700

    Agregar archivos de proyecto.

---
+++ App3/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Envia_Clicked(object sender, EventArgs e)
+        {
+            string nombre = nombretext.Text;
+            string apellido = Apellitext.Text;
+            int edad = int.Parse(Edadtext.Text);
+            string correo = Correotext.Text;
+            await Application.Current.MainPage.Navigation.PushModalAsync(new Pagina2(nombre,apellido,edad,correo));    
+
+        }
+    }
+}
---
+++ App3/Pagina2.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pagina2 : ContentPage
+	{
+		public Pagina2 (string nombre, string apellido, int edad, string correo)
+		{
+			InitializeComponent ();
+			INFO.Text = nombre + apellido + edad + correo;
+
+
+        }
+    }
+}
